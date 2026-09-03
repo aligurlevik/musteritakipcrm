343f3b8686f1b25e27a5288118ca77bfb7cd25c5
aligurlevik <aagurlevik@gmail.com>
2026-09-03 18:45:02 +0300

    Mobil Ajanda yönlendirme döngüsünü düzelt

--- src/mobile_agenda_redirect_patch.js
+++ src/mobile_agenda_redirect_patch.js
@@ -7,17 +7,31 @@ function isMobileRequest(request){
   return /Android|iPhone|iPad|iPod|Mobile|IEMobile|Opera Mini/i.test(ua);
 }
 
+function mobileAgendaRequest(request){
+  const assetUrl=new URL(request.url);
+  // Cloudflare Static Assets'in kanonik HTML yolu /mobil-ajanda'dır.
+  // .html kullanılırsa 307 ile /mobil-ajanda'ya yönlenir ve eski katmanla döngü oluşabilir.
+  assetUrl.pathname='/mobil-ajanda';
+  assetUrl.search='';
+  return new Request(assetUrl.toString(),request);
+}
+
 export default {
   async fetch(request,env,ctx){
     const url=new URL(request.url);
-    const isHome=request.method==='GET'&&(url.pathname==='/'||url.pathname==='/index.html');
+    const isGet=request.method==='GET';
+    const isHome=isGet&&(url.pathname==='/'||url.pathname==='/index.html');
+    const isMobilePage=isGet&&(url.pathname==='/mobil-ajanda'||url.pathname==='/mobil-ajanda.html');
     const forceMobile=url.searchParams.get('mobile')==='1';
 
+    // Mobil Ajanda yolunu en üst katmanda doğrudan statik asset'ten servis et.
+    // Böylece alttaki eski /mobil-ajanda.html işleyicisine hiç düşmez ve yönlendirme döngüsü oluşmaz.
+    if(isMobilePage){
+      return env.ASSETS.fetch(mobileAgendaRequest(request));
+    }
+
     if(isHome&&(forceMobile||isMobileRequest(request))){
-      const assetUrl=new URL(request.url);
-      assetUrl.pathname='/mobil-ajanda.html';
-      assetUrl.search='';
-      return env.ASSETS.fetch(new Request(assetUrl.toString(),request));
+      return env.ASSETS.fetch(mobileAgendaRequest(request));
     }
 
     return worker.fetch(request,env,ctx);
